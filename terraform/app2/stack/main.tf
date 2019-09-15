8087a11387148f2a256f2e6fe963e13b103f843a
jayaprakash reddy <jayaprakash.jp63@gmail.com>
2019-09-15 23:58:31 +0530

    included nginx container

--- terraform/app2/stack/main.tf
+++ terraform/app2/stack/main.tf
@@ -1,10 +1,23 @@
 # Create a container
-resource "docker_container" "docker" {
-  image = "${docker_image.ubuntu.latest}"
-  name  = "${var.application}-${var.environment}-flux-demo"
-  rm = true
+# resource "docker_container" "docker" {
+#   image = "${docker_image.ubuntu.latest}"
+#   name  = "${var.application}-${var.environment}-flux-demo"
+#   rm = true
+# }
+                
+resource "docker_container" "nginx-server" {
+  name =  "${var.application}-${var.environment}-flux-demo"
+  image = "${docker_image.nginx.latest}"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path  = "/usr/share/nginx/html"
+    host_path = "/var/lib/nginx"
+    read_only = true
+  }
 }
 
-resource "docker_image" "ubuntu" {
-  name = "docker.io/ubuntu:${var.docker_image_version}"
+resource "docker_image" "nginx" {
+  name = "docker.io/nginx:${var.docker_image_version}"
 }
